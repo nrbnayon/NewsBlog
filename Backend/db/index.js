11cc05bf71b92d1217cad8209db0c699f315bf7f
nrbnayon <nrbnayon@gmail.com>
2023-04-29 01:21:20 +0600

    Final Update

--- Backend/db/index.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose
-    .connect('mongodb+srv://nrbnayon:nrbnayon@nbapp.inund6i.mongodb.net/news-blog?retryWrites=true&w=majority')
-    .then(() => console.log(" DB Connected "))
-    .catch((err) => console.log("Db connection Failed ", err.message || err));
-        // .connect('mongodb://127.0.0.1:27017/news-blog')
